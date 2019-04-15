88c3b81892164b6fc5ec94537aaca15a0e7a6892
Ivan Gulakov <gulakovthecreator@gmail.com>
2019-04-15 19:57:29 +0300

    start/build update

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 go build 8/main.go;
 go build 4/main.go;
-dotnet build 1/..;
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-ruby 3/app.rb 2>&1 | tee logs/3.txt
-./8/main 2>&1 | tee logs/8.txt
-./4/main 2>&1 | tee logs/4.txt
+ruby 3/app.rb 2>&1 | tee logs/3.txt;
+./8/main 2>&1 | tee logs/8.txt;
+./4/main 2>&1 | tee logs/4.txt;
+dotnet run ./1/FlightPassengerApi/FlightPassengerApi.csproj 2>&1 | tee logs/1.txt;

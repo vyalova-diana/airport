a39e8aad4ff4768ae1c68dcfc1f6f6b1f5ccb777
Likka123 <novichkova_lera@mail.ru>
2019-04-15 03:03:10 +0300

    comment

---
+++ Airport_Check_In/WebApiGui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

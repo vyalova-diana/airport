b4ed84544841f71e003de1782f35d780fe2a6d03
Ivan Gulakov <gulakovthecreator@gmail.com>
2019-04-18 01:09:12 +0300

    3rd service code

--- 8/app.py
+++ 8/app.py
@@ -1,18 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/car")
-def hello():
-    return "Hello World!"
+# Availiable status for the plane:
+# - "arrived"
+# - "waiting_follow"
+# - ""
+class Plane:
+    def __init__(self, ID):
+        self.ID = ID
+        self.status = "arrived"
 
-@app.route("/car/*ID*/sendToStorage", methods=['POST'])
-def hello():
-    return "Hello World!"
-
-@app.route("/car/*ID1*/sendToAirplane/*ID2*", methods=['POST'])
-def hello():
-    return "Hello World!"
-
-@app.route("/cargos", methods=['POST', 'GET'])
-def hello():
-    return "Hello World!"
+def ask_follow_me():
+    return ""

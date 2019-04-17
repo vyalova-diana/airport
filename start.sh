650c45345cf85a7c96f37bb08831a62251b31a46
Ivan Gulakov <gulakovthecreator@gmail.com>
2019-04-17 19:40:29 +0300

    build script update

--- start.sh
+++ start.sh
@@ -19,15 +19,14 @@ rm 16.txt
 killall dotnet
 python3 -m http.server &
 cd ..
-ruby 4/app.rb 2>&1 | tee logs/3.txt \
-&& python3 3/app.py 2>&1 | tee logs/4.txt \
-&& python3 8/app.py 2>&1 | tee logs/8.txt \
-&& dotnet run --project ./1/FlightPassengerApi/FlightPassengerApi.csproj 2>&1 | tee logs/1.txt \
-&& dotnet run --project ./2/BusApi/BusApi.csproj 2>&1 | tee logs/2.txt \
-&& dotnet run --project ./2/BusApi/BusApi.csproj 2>&1 | tee logs/2.txt \
-&& dotnet run --project ./5/RefuellerBackend/RefuelBackend.csproj 2>&1 | tee logs/5.txt \
-&& dotnet run --project ./6/FollowMeService/FollowMeService.csproj | tee logs/6.txt \
-&& dotnet run --project ./13/WebApi/WebApi/WebApi.csproj | tee logs/13.txt1 \
-&& dotnet run --project ./16/TicketWindow/TicketWindow.csproj | tee logs/16.txt \
-&& dotnet run --project ./Airport_Check_In/WebApplication_CheckIn/WebApplication_CheckIn.csproj | tee logs/9.txt1 \
-&& dotnet run --project ./Airport_tablo/WebApiAirportNew/WebApiAirportNew.csproj | tee logs/15.txt
+ruby 4/app.rb 2>&1 | tee logs/3.txt;
+python3 3/app.py 2>&1 | tee logs/4.txt &
+python3 8/app.py 2>&1 | tee logs/8.txt &
+dotnet run --project ./2/BusApi/BusApi.csproj 2>&1 | tee logs/2.txt &
+dotnet run --project ./1/FlightPassengerApi/FlightPassengerApi.csproj 2>&1 | tee logs/1.txt &
+dotnet run --project ./5/RefuellerBackend/RefuelBackend.csproj 2>&1 | tee logs/5.txt &
+dotnet run --project ./6/FollowMeService/FollowMeService.csproj 2>&1 | tee logs/6.txt &
+dotnet run --project ./13/WebApi/WebApi/WebApi.csproj 2>&1 | tee logs/13.txt1 &
+dotnet run --project ./16/TicketWindow/TicketWindow.csproj 2>&1 | tee logs/16.txt &
+dotnet run --project ./Airport_Check_In/WebApplication_CheckIn/WebApplication_CheckIn.csproj 2>&1 | tee logs/9.txt1 &
+dotnet run --project ./Airport_tablo/WebApiAirportNew/WebApiAirportNew.csproj 2>&1 | tee logs/15.txt &

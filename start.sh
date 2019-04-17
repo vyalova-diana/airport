7dc23030a3741fbf961cd1b8dfe1add2877c0308
Ivan Gulakov <gulakovthecreator@gmail.com>
2019-04-17 20:16:54 +0300

    Possible fix

--- start.sh
+++ start.sh
@@ -17,7 +17,6 @@ rm 14.txt
 rm 15.txt
 rm 16.txt
 killall dotnet
-python3 -m http.server 3221 &
 cd ..
 ruby 4/app.rb 2>&1 | tee logs/3.txt;
 python3 3/app.py 2>&1 | tee logs/4.txt &
@@ -30,3 +29,4 @@ dotnet run --project ./13/WebApi/WebApi/WebApi.csproj 2>&1 | tee logs/13.txt1 &
 dotnet run --project ./16/TicketWindow/TicketWindow.csproj 2>&1 | tee logs/16.txt &
 dotnet run --project ./Airport_Check_In/WebApplication_CheckIn/WebApplication_CheckIn.csproj 2>&1 | tee logs/9.txt1 &
 dotnet run --project ./Airport_tablo/WebApiAirportNew/WebApiAirportNew.csproj 2>&1 | tee logs/15.txt &
+python3 -m http.server 3221

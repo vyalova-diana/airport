c830e37cd4f7fa23afbfd29043fdc87ee6273da0
kykuruza <41751111+kykuruza@users.noreply.github.com>
2019-04-18 12:15:23 +0300

    Update Program.cs

--- 13/Airport/Program.cs
+++ 13/Airport/Program.cs
@@ -14,14 +14,14 @@ namespace Airport
     {
         public static void Main(string[] args)
         {
-            MyTime MT = MyTime.GetMyTime();
+            //MyTime MT = MyTime.GetMyTime();
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
         .UseKestrel()
-        .UseUrls("http://localhost:61120/")
+        .UseUrls("http://localhost:7013/")
         .UseStartup<Startup>();
     }
 }
